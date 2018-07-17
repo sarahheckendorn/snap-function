393b130f811ddef8c306f27263c5fbcb57d63f41
Sarah Heckendorn <praisinghim@jcangus.com>
2018-07-17 08:47:35 -0600

    fixed age

--- function.php
+++ function.php
@@ -1,7 +1,8 @@
 <?php
 
 function assocArray(){
-	$array = $assocArray(array("Caleb" => 18, "Rebecca" => 18, "Josh" => 22))
+	$array = $assocArray(array("Caleb" => 18, "Rebecca" => 27, "Josh" => 22));
+
 
 print_r()
 echo $x /= 3;
